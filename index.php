9ce97708384d7bb4deb996d881a028450f0d567b
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-17 10:24:10 +0100

    index, bootstrap CDN

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require 'class/Autoloader.php';
+Autoloader::register();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <title>Bataillons</title>
+</head>
+<body>
+    
+    <!-- Latest compiled and minified JS -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
+
